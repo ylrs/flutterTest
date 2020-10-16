2e7ecec804a74ddb80afc9018bd781a997f1149c
zhulongfei001 <zhulongfei001@ke.com>
2020-10-16 15:57:17 +0800

    增加工程

---
+++ flutter_plugin_test/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_plugin_test/FlutterPluginTestPlugin.h>)
+#import <flutter_plugin_test/FlutterPluginTestPlugin.h>
+#else
+@import flutter_plugin_test;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterPluginTestPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterPluginTestPlugin"]];
+}
+
+@end
